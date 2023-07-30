659875e8915dfb35f8cbf34ce35a7d9ebe832ca7
hmourad <hazemmgaber3110@gmail.com>
2023-07-30 03:34:57 +0300

    Project Final Image

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace AI_Chat__C_Framework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.rTxtResult = new System.Windows.Forms.RichTextBox();
+            this.txtAskFor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(651, 10);
+            this.btnGenerate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(202, 62);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // rTxtResult
+            // 
+            this.rTxtResult.Location = new System.Drawing.Point(10, 145);
+            this.rTxtResult.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rTxtResult.Name = "rTxtResult";
+            this.rTxtResult.Size = new System.Drawing.Size(596, 203);
+            this.rTxtResult.TabIndex = 3;
+            this.rTxtResult.Text = "";
+            this.rTxtResult.TextChanged += new System.EventHandler(this.rTxtResult_TextChanged);
+            // 
+            // txtAskFor
+            // 
+            this.txtAskFor.Location = new System.Drawing.Point(58, 10);
+            this.txtAskFor.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtAskFor.Name = "txtAskFor";
+            this.txtAskFor.Size = new System.Drawing.Size(548, 20);
+            this.txtAskFor.TabIndex = 4;
+            this.txtAskFor.TextChanged += new System.EventHandler(this.txtAskFor_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Result";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Ask for";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 357);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAskFor);
+            this.Controls.Add(this.rTxtResult);
+            this.Controls.Add(this.btnGenerate);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.RichTextBox rTxtResult;
+        private System.Windows.Forms.TextBox txtAskFor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,232 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Keys = OpenQA.Selenium.Keys;
+
+namespace AI_Chat__C_Framework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void bingProcess()
+        {
+            
+
+
+
+        }
+
+
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            string inputText = txtAskFor.Text;
+            var options = new ChromeOptions();
+            options.AddArgument("--headless");
+            options.AddArgument("--disable-site-isolation-trials");
+            options.AddArgument("--disable-features=NetworkService,NetworkServiceInProcess");
+            options.AddArgument("--allow-cross-origin-auth-prompt");
+            options.AddArgument("--allow-reading-third-party-cookies");
+            options.AddArgument("--allow-third-party-cookies");
+
+
+            try {
+
+                         var driver = new ChromeDriver(options);
+
+                try
+                {
+
+                    // Navigate to website and login
+                    driver.Url = "https://www.koala.sh";
+                    IWait<IWebDriver> loading = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+                    var loginbtn = driver.FindElement(By.XPath("//body/div[@id='__next']/div[1]/div[1]/div[1]/a[5]"));
+                    loginbtn.Click();
+                    IWait<IWebDriver> wait1 = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+                    var mail = driver.FindElement(By.CssSelector("#email"));
+                    mail.SendKeys("lerid25798@aaorsi.com"); //test  temp mail 
+                    var password = driver.FindElement(By.CssSelector("#password"));
+                    password.SendKeys("roadstar2000"); //test temp password 
+                    var loginbtnforum = driver.FindElement(By.XPath("//button[contains(text(),'Login')]"));
+                    loginbtnforum.Submit();
+                    IEnumerable<OpenQA.Selenium.Cookie> cookies = driver.Manage().Cookies.AllCookies;
+                    SaveCookiesToFile(cookies, "cookies.txt");
+                    // Later, restore the session using the saved cookies
+
+                    //  IWait<IWebDriver> wtime = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+                    ChromeDriver driver1 = RestoreSessionFromCookiesFile("cookies.txt");
+                    driver1.Quit();
+                    var txtarea = driver.FindElement(By.TagName("textarea"));
+                    txtarea.SendKeys(inputText);
+                    txtarea.SendKeys(Keys.Enter);
+                    Thread.Sleep(15000);
+
+                    var txtchat = driver.FindElement(By.XPath("//body[1]/div[1]/div[1]/div[2]/main[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/p[1]"));
+                    String chats = txtchat.Text;
+                    string text = txtchat.GetAttribute("innerText");
+                    rTxtResult.Text = text+"\n";
+
+                    driver.Quit();
+
+                }
+
+
+                catch (NoSuchElementException)
+                {
+
+                    IWebDriver driver2 = new ChromeDriver(options);
+                    driver2.Url = "https://shuffle.dev/chat";
+                    var inputTxt = driver2.FindElement(By.CssSelector("#chat-message-input"));
+                    inputTxt.Click();
+                    inputTxt.SendKeys(inputText);
+                    inputTxt.SendKeys(Keys.Enter);
+                    Thread.Sleep(20000);
+                    var textchat = driver2.FindElement(By.XPath("/html/body/section/div[2]/div[2]/div[2]/div[5]/div/div[1]/div/div[2]/div/div/div/p"));
+                    String chats = textchat.Text;
+                    string text = textchat.GetAttribute("innerText");
+                    rTxtResult.Text = text;
+
+          
+                  
+                     driver2.Quit();
+
+
+                }
+
+
+
+            }
+
+
+
+            catch (NoSuchElementException)
+            {
+                IWebDriver driver2 = new ChromeDriver(options);
+                driver2.Quit();
+
+                // Create a new instance of ChromeDriver
+                IWebDriver driver3 = new ChromeDriver(options);
+
+                // Navigate to www.you.com
+                driver3.Navigate().GoToUrl("https://www.you.com");
+                Thread.Sleep(5000);
+                var textbox = driver3.FindElement(By.XPath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[3]/section[1]/main[1]/div[3]/div[2]/div[1]/div[3]/div[1]/div[1]/div[1]/textarea[1]"));
+                textbox.Click();
+                var textsend = driver3.FindElement(By.TagName("textarea"));
+                textsend.SendKeys(inputText);
+                textsend.SendKeys(Keys.Enter);
+                Thread.Sleep(10000);
+                var chats = driver3.FindElements(By.XPath("//p[@class='sc-98942fc5-0 JcSiB']"));
+
+                // Iterate through the elements and get their full text
+                foreach (var chat in chats)
+                {
+                    string text = chat.GetAttribute("innerText");
+                    rTxtResult.Text = text + "\n";
+                }
+                // Close the browser
+                driver3.Quit();
+            }
+                
+
+            }
+           
+        private void txtAskFor_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rTxtResult_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        static ChromeDriver RestoreSessionFromCookiesFile(string filePath)
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--headless");
+
+            ChromeDriver driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("https://www.koala.sh/");
+
+            // Load cookies from file
+            IEnumerable<OpenQA.Selenium.Cookie> cookies = LoadCookiesFromFile(filePath);
+
+            // Add cookies to the current session
+            foreach (OpenQA.Selenium.Cookie cookie in cookies)
+            {
+                driver.Manage().Cookies.AddCookie(cookie);
+            }
+
+            // Refresh the page to apply the cookies
+            driver.Navigate().Refresh();
+
+            return driver;
+        }
+
+        static bool SaveCookiesToFile(IEnumerable<OpenQA.Selenium.Cookie> cookies, string filePath)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    foreach (OpenQA.Selenium.Cookie cookie in cookies)
+                    {
+                        writer.WriteLine($"{cookie.Name}|{cookie.Value}|{cookie.Domain}|{cookie.Path}|{cookie.Expiry}");
+                    }
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error saving cookies to file: {ex.Message}");
+                return false;
+            }
+        }
+
+        static IEnumerable<OpenQA.Selenium.Cookie> LoadCookiesFromFile(string filePath)
+        {
+            List<OpenQA.Selenium.Cookie> cookies = new List<OpenQA.Selenium.Cookie>();
+
+            using (System.IO.StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] parts = line.Split('|');
+                    OpenQA.Selenium.Cookie cookie = new OpenQA.Selenium.Cookie(parts[0], parts[1], parts[2], parts[3], DateTime.Parse(parts[4]));
+                    cookies.Add(cookie);
+                }
+            }
+
+            return cookies;
+        }
+
+
+
+
+    }
+}
